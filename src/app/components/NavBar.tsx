d88c65bd5c559aad11d454c430c53ce59e0119e7
jose <jruizzapata38@gmail.com>
2025-09-01 13:41:08 -0500

    Se agrego un nuevo archivo tipo tsx en la carpeta componentes, con un codigo que funciona para la barra inicial de la aplicacion

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <nav className="bg-blue-900 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-xl font-bold">Hotel JSJ</div>
+        <div className="space-x-6">
+          <Link href="/" className="hover:text-blue-300">Inicio</Link>
+          <Link href="/rooms" className="hover:text-blue-300">Habitaciones</Link>
+          <Link href="/about" className="hover:text-blue-300">Sobre Nosotros</Link>
+          <Link href="/help" className="hover:text-blue-300">Ayuda</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
